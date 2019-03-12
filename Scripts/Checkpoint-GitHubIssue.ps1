3057fce08949ee4738b38fe280084cccbe7c207d
apobekiaris <apostolis.bekiaris@gmail.com>
2019-03-12 14:55:50 +0200

    Update Checkpoint-GitHubIssue message

--- Scripts/Checkpoint-GitHubIssue.ps1
+++ Scripts/Checkpoint-GitHubIssue.ps1
@@ -8,6 +8,6 @@ $version=Get-XpandVersion -Lab
 Write-Verbose -Verbose "##vso[build.updatebuildnumber]$version"
 $nugetServerUri="https://xpandnugetserver.azurewebsites.net/"
 $installerBuildUri="https://github.com/eXpandFramework/lab/releases/$version"
-$msg="Installer lab build [$version]($installerBuildUri) includes commit {Commits} that relate to this task. Please test if it addresses the problem. If you use nuget add our [NugetServer]({$nugetServerUri}) as a nuget package source in VS"
+$msg="Installer lab build [$version]($installerBuildUri) includes commit {Commits} that relate to this task. Please test if it addresses the problem. If you use nuget add our [NugetServer]($nugetServerUri) as a nuget package source in VS.`r`n`r`n. Thanks a lot for your contribution."
 $msg
 Checkpoint-GithubIssue -GitHubApp eXpandFramework -Owner $GithubUserName -Organization eXpandFramework -Repository1 eXpand -Repository2 "lab" -Message $msg -Pass $GithubPass
