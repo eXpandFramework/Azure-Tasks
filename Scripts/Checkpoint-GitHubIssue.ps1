2a838914b87e6401e64571af5216394083fa08aa
apobekiaris <apostolis.bekiaris@gmail.com>
2019-03-13 19:02:35 +0200

    XpandPosh 1.0.50 for Checkpoint-GitHubIssue

--- Scripts/Checkpoint-GitHubIssue.ps1
+++ Scripts/Checkpoint-GitHubIssue.ps1
@@ -1,13 +1,21 @@
 param(
     $GithubUserName,
-    $GithubPass
+    $GithubPass 
 )
-$VerbosePreference="continue"
-Install-Module XpandPosh -RequiredVersion 1.0.46 -Scope CurrentUser -Force -Repository PSGallery 
-$version=Get-XpandVersion -Lab 
+$VerbosePreference = "continue"
+Install-Module XpandPosh -RequiredVersion 1.0.50 -Scope CurrentUser -Force -Repository PSGallery 
+$version = Get-XpandVersion -Lab 
+$version
+$cred = @{
+    Owner        = $GitHubUserName 
+    Pass         = $GitHubPass 
+    Organization = "eXpandFramework"
+}
+
+$commitIssues = Get-GitHubCommitIssue -Repository1 eXpand -Repository2 lab @cred
+$commitIssues
 Write-Verbose -Verbose "##vso[build.updatebuildnumber]$version"
-$nugetServerUri="https://xpandnugetserver.azurewebsites.net/"
-$installerBuildUri="https://github.com/eXpandFramework/lab/releases/$version"
-$msg="Installer lab build [$version]($installerBuildUri) includes commit {Commits} that relate to this task. Please test if it addresses the problem. If you use nuget add our [NugetServer]($nugetServerUri) as a nuget package source in VS.`r`n`r`n. Thanks a lot for your contribution."
+
+$msg = "Installer lab build [$version](https://github.com/eXpandFramework/lab/releases/$version) includes commit {Commits} that relate to this task. Please test if it addresses the problem. If you use nuget add our [NugetServer](https://xpandnugetserver.azurewebsites.net/) as a nuget package source in VS.`r`n`r`n. Thanks a lot for your contribution."
 $msg
-Checkpoint-GithubIssue -GitHubApp eXpandFramework -Owner $GithubUserName -Organization eXpandFramework -Repository1 eXpand -Repository2 "lab" -Message $msg -Pass $GithubPass
+Checkpoint-GithubIssue -CommitIssues $commitIssues -Message $msg @cred -WhatIf
