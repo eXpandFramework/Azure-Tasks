666b76b09e9063a3e8f1b0bb1b6e3def645f8a5a
apobekiaris <apostolis.bekiaris@gmail.com>
2019-03-14 13:49:34 +0200

    Install-module

--- Scripts/Install-Module.ps1
+++ Scripts/Install-Module.ps1
@@ -1,14 +1,12 @@
+param(
+    [string]$Yaml
+)
 if (!(Get-Module powershell-yaml -ListAvailable)){
     Install-Module powershell-yaml -RequiredVersion 0.4.0 -Scope CurrentUser -Force -Repository PSGallery 
     Import-Module powershell-yaml
 }
 
-(@"
-- Name: XpandPosh
-  Version: 1.0.48
-- Name: VSTeam
-  Version: 6.1.2
-"@|ConvertFrom-Yaml)|ForEach-Object{
+($Yaml|ConvertFrom-Yaml)|ForEach-Object{
     if (!(Get-Module $_.Name -ListAvailable)){
         Install-Module $_.Name -RequiredVersion $_.Version -Scope CurrentUser -Force -Repository PSGallery 
     }
