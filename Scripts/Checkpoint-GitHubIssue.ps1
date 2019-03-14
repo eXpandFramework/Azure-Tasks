004dc350acc8d5848d0108f880da5b2e4e766728
apobekiaris <apostolis.bekiaris@gmail.com>
2019-03-14 20:07:14 +0200

    Checkpoint-GitHubIssue fails is no issues found

--- Scripts/Checkpoint-GitHubIssue.ps1
+++ Scripts/Checkpoint-GitHubIssue.ps1
@@ -3,7 +3,7 @@ param(
     $GithubPass 
 )
 $VerbosePreference = "continue"
-Install-Module XpandPosh -RequiredVersion 1.1.0 -Scope CurrentUser -Force -Repository PSGallery 
+Install-Module XpandPosh -RequiredVersion 1.1.5 -Scope CurrentUser -Force -Repository PSGallery 
 $version = Get-XpandVersion -Lab 
 $version
 $cred = @{
@@ -14,12 +14,14 @@ $cred = @{
 
 $commitIssues = Get-GitHubCommitIssue -Repository1 eXpand -Repository2 lab @cred
 $commitIssues
-$milestone=Get-GitHubMilestone -Repository eXpand -Latest @cred
-$milestone
-Write-Verbose -Verbose "##vso[build.updatebuildnumber]$version"
+if ($commitIssues) {
+    $milestone = Get-GitHubMilestone -Repository eXpand -Latest @cred
+    $milestone
+    Write-Verbose -Verbose "##vso[build.updatebuildnumber]$version"
 
-$msg = "Installer lab build [$version](https://github.com/eXpandFramework/lab/releases/$version) includes commit {Commits} that relate to this task. Please test if it addresses the problem. If you use nuget add our [NugetServer](https://xpandnugetserver.azurewebsites.net/) as a nuget package source in VS.`r`n`r`n. Thanks a lot for your contribution."
-$msg
-Checkpoint-GithubIssue -CommitIssues $commitIssues -Message $msg @cred |ForEach-Object{
-    Update-GitHubIssue -IssueNumber $_.Number -Repository eXpand -MileStoneTitle $milestone
+    $msg = "Installer lab build [$version](https://github.com/eXpandFramework/lab/releases/$version) includes commit {Commits} that relate to this task. Please test if it addresses the problem. If you use nuget add our [NugetServer](https://xpandnugetserver.azurewebsites.net/) as a nuget package source in VS.`r`n`r`n. Thanks a lot for your contribution."
+    $msg
+    Checkpoint-GithubIssue -CommitIssues $commitIssues -Message $msg @cred |ForEach-Object {
+        Update-GitHubIssue -IssueNumber $_.Number -Repository eXpand -MileStoneTitle $milestone
+    }
 }
