198073a1e682f6f07aa2225fb67ff24f3f1378ac
apobekiaris <apostolis.bekiaris@gmail.com>
2019-03-15 05:29:05 +0200

    Checkpoint-GitHubIssue fails when no issues

--- Scripts/Checkpoint-GitHubIssue.ps1
+++ Scripts/Checkpoint-GitHubIssue.ps1
@@ -1,9 +1,13 @@
 param(
-    $GithubUserName ,
-    $GithubPass 
+    $GithubUserName ="apobekiaris",
+    $GithubPass =$env:GithubPass
 )
 $VerbosePreference = "continue"
-Install-Module XpandPosh -RequiredVersion 1.1.5 -Scope CurrentUser -Force -Repository PSGallery 
+$yaml = @"
+- Name: XpandPosh
+  Version: 1.1.5
+"@
+& "$PSScriptRoot\Install-Module.ps1" $yaml
 $version = Get-XpandVersion -Lab 
 $version
 $cred = @{
@@ -11,8 +15,12 @@ $cred = @{
     Pass         = $GitHubPass 
     Organization = "eXpandFramework"
 }
-
-$commitIssues = Get-GitHubCommitIssue -Repository1 eXpand -Repository2 lab @cred
+$commitArgs=@{
+    Repository1="eXpand"
+    Repository2="lab"
+}+$cred
+$commitArgs
+$commitIssues = Get-GitHubCommitIssue @commitArgs
 $commitIssues
 if ($commitIssues) {
     $milestone = Get-GitHubMilestone -Repository eXpand -Latest @cred
@@ -21,7 +29,9 @@ if ($commitIssues) {
 
     $msg = "Installer lab build [$version](https://github.com/eXpandFramework/lab/releases/$version) includes commit {Commits} that relate to this task. Please test if it addresses the problem. If you use nuget add our [NugetServer](https://xpandnugetserver.azurewebsites.net/) as a nuget package source in VS.`r`n`r`n. Thanks a lot for your contribution."
     $msg
-    Checkpoint-GithubIssue -CommitIssues $commitIssues -Message $msg @cred |ForEach-Object {
-        Update-GitHubIssue -IssueNumber $_.Number -Repository eXpand -MileStoneTitle $milestone
+    Checkpoint-GithubIssue -CommitIssues $commitIssues -Message $msg @cred -WhatIf |ForEach-Object {
+        if ($_){
+            Update-GitHubIssue -IssueNumber $_.Number -Repository eXpand -MileStoneTitle $milestone
+        }
     }
 }
