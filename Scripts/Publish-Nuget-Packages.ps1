dfb6b35899e3128fdaabf25567cc0e17dca09c3a
apobekiaris <apostolis.bekiaris@gmail.com>
2019-03-20 10:19:05 +0200

    XpandPosh 1.3.20

--- Scripts/Publish-Nuget-Packages.ps1
+++ Scripts/Publish-Nuget-Packages.ps1
@@ -7,14 +7,14 @@ $VerbosePreference="continue"
 
 $yaml = @"
 - Name: XpandPosh
-  Version: 1.3.4
+  Version: 1.3.20
 - Name: VSTeam
   Version: 6.1.2
 "@
 & "$PSScriptRoot\Install-Module.ps1" $yaml
 Set-VSTeamAccount -Account eXpandDevOps -PersonalAccessToken $AzureToken
 
-$publishNugetFeed = "https://api.nuget.org/v3/index.json"
+$publishNugetFeed = Get-PackageFeed -Xpand
 $labBuild = Get-VSTeamBuild -ProjectName eXpandFramework|Where-Object {$_.DefinitionName -eq "Xpand-Lab" -and $_.Result -eq "succeeded"}|Select-Object -first 1
 $releaseBuild = Get-VSTeamBuild -ProjectName eXpandFramework|Where-Object {$_.DefinitionName -eq "Xpand-Release" -and $_.Result -eq "succeeded"}|Select-Object -first 1
 $labBuild.buildNumber
@@ -24,9 +24,10 @@ $build=$labBuild
 if ((new-object System.Version($releaseBuild.buildNumber)) -gt (new-object System.Version($labBuild.buildNumber))){
     $version = $releaseBuild.BuildNumber
     $build=$releaseBuild
-    $publishNugetFeed = "https://api.nuget.org/v3/index.json"
+    $publishNugetFeed = Get-PackageFeed -Nuget
 }
 "version=$version"
+"publishNugetFeed=$publishNugetFeed"
 Write-Verbose -Verbose "##vso[build.updatebuildnumber]$version"
 $a = Get-VSTeamBuildArtifact -Id $build.id -ProjectName eXpandFramework -ErrorAction Continue
 $uri="$($a.resource.downloadUrl)"
