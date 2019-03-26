434b66ca777c55f00f9897ab82dc93e38a12d46f
apobekiaris <apostolis.bekiaris@gmail.com>
2019-03-26 15:06:44 +0200

    Checkpoint-GithubIssue support the DevExpress.XAF project

--- Scripts/Checkpoint-GitHubIssue.ps1
+++ Scripts/Checkpoint-GitHubIssue.ps1
@@ -1,40 +1,56 @@
 param(
-    $GithubUserName ="apobekiaris",
-    $GithubPass =$env:GithubPass
+    $GithubUserName ,
+    $GithubPass ,
+    $BuildRepository
 )
 $VerbosePreference = "continue"
 $yaml = @"
 - Name: XpandPosh
-  Version: 1.1.5
+  Version: 1.5.3
 "@
 & "$PSScriptRoot\Install-Module.ps1" $yaml
-$version = Get-XpandVersion -Lab 
-$version
-$cred = @{
-    Owner        = $GitHubUserName 
-    Pass         = $GitHubPass 
-    Organization = "eXpandFramework"
-}
-$commitArgs=@{
-    Repository1="eXpand"
-    Repository2="lab"
-}+$cred
-$commitArgs
-$commitIssues = Get-GitHubCommitIssue @commitArgs
-$commitIssues
-if ($commitIssues) {
-    $milestone = Get-GitHubMilestone -Repository eXpand -Latest @cred
-    $milestone
-    Write-Verbose -Verbose "##vso[build.updatebuildnumber]$version"
-
+if ($BuildRepository -eq "DevExpress.XAF"){
+    "The $BuildRepository includes commit {Commits} that relate to this task. Please update the related Nuget packages and test if issues is addressed. These are nightly nuget packages available only from our [NugetServer](https://xpandnugetserver.azurewebsites.net/)."
     $msg = "Installer lab build [$version](https://github.com/eXpandFramework/lab/releases/$version) includes commit {Commits} that relate to this task. Please test if it addresses the problem. If you use nuget add our `LAB` [NugetServer](https://xpandnugetserver.azurewebsites.net/) as a nuget package source in VS.`r`n`r`n. Thanks a lot for your contribution."
     $msg
-    Checkpoint-GithubIssue -CommitIssues $commitIssues -Message $msg @cred |ForEach-Object {
-        if ($_){
-            $_
-            if ($_.Number){
-                Update-GitHubIssue -IssueNumber $_.Number -Repository eXpand -MileStoneTitle $milestone
+    $Branch="lab"    
+}
+else{
+    $version = Get-XpandVersion -Lab 
+    $version
+    Write-Verbose -Verbose "##vso[build.updatebuildnumber]$version"
+    $msg = "Installer lab build [$version](https://github.com/eXpandFramework/lab/releases/$version) includes commit {Commits} that relate to this task. Please test if it addresses the problem. If you use nuget add our `LAB` [NugetServer](https://xpandnugetserver.azurewebsites.net/) as a nuget package source in VS.`r`n`r`n. Thanks a lot for your contribution."
+    $msg
+}
+
+function UpdateIssues ($Repository,$Branch) {
+    
+    $cred = @{
+        Owner        = $GitHubUserName 
+        Pass         = $GitHubPass 
+        Organization = "eXpandFramework"
+    }
+
+    $commitArgs = @{
+        Repository1 = "eXpand"
+        Repository2 = $Repository
+        Branch=$Branch
+    } + $cred
+    $commitArgs
+    $commitIssues = Get-GitHubCommitIssue @commitArgs 
+    $commitIssues
+    if ($commitIssues) {
+        $milestone = Get-GitHubMilestone -Repository eXpand -Latest @cred
+        $milestone
+        Checkpoint-GithubIssue -CommitIssues $commitIssues -Message $msg @cred |ForEach-Object {
+            if ($_) {
+                $_
+                if ($_.Number) {
+                    Update-GitHubIssue -IssueNumber $_.Number -Repository eXpand -MileStoneTitle $milestone
+                }
             }
         }
     }
 }
+
+UpdateIssues $Repository $Branch
