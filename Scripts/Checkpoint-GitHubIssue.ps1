efa9bc1688b661b4c78266a7d848db3b813d9876
apobekiaris <apostolis.bekiaris@gmail.com>
2019-03-27 07:54:48 +0200

    Checkpoint-GitHubIssue update message

--- Scripts/Checkpoint-GitHubIssue.ps1
+++ Scripts/Checkpoint-GitHubIssue.ps1
@@ -9,20 +9,6 @@ $yaml = @"
   Version: 1.5.3
 "@
 & "$PSScriptRoot\Install-Module.ps1" $yaml
-if ($BuildRepository -eq "DevExpress.XAF"){
-    "The $BuildRepository includes commit {Commits} that relate to this task. Please update the related Nuget packages and test if issues is addressed. These are nightly nuget packages available only from our [NugetServer](https://xpandnugetserver.azurewebsites.net/)."
-    $msg = "Installer lab build [$version](https://github.com/eXpandFramework/lab/releases/$version) includes commit {Commits} that relate to this task. Please test if it addresses the problem. If you use nuget add our `LAB` [NugetServer](https://xpandnugetserver.azurewebsites.net/) as a nuget package source in VS.`r`n`r`n. Thanks a lot for your contribution."
-    $msg
-    $Branch="lab"    
-}
-else{
-    $version = Get-XpandVersion -Lab 
-    $version
-    Write-Verbose -Verbose "##vso[build.updatebuildnumber]$version"
-    $msg = "Installer lab build [$version](https://github.com/eXpandFramework/lab/releases/$version) includes commit {Commits} that relate to this task. Please test if it addresses the problem. If you use nuget add our `LAB` [NugetServer](https://xpandnugetserver.azurewebsites.net/) as a nuget package source in VS.`r`n`r`n. Thanks a lot for your contribution."
-    $msg
-}
-
 function UpdateIssues ($Repository,$Branch) {
     
     $cred = @{
@@ -53,4 +39,11 @@ function UpdateIssues ($Repository,$Branch) {
     }
 }
 
-UpdateIssues $Repository $Branch
+
+$msg="The DevExpress.XAF includes commit {Commits} that relate to this task. Please update the related Nuget packages and test if issues is addressed. These are nightly nuget packages available only from our [NugetServer](https://xpandnugetserver.azurewebsites.net/)."
+UpdateIssues "DevExpress.XAF" "lab"
+
+$version = Get-XpandVersion -Lab 
+$msg = "eXpand.lab Installer [$version](https://github.com/eXpandFramework/lab/releases/$version) includes commit {Commits} that relate to this task. Please test if it addresses the problem. If you use nuget add our `LAB` [NugetServer](https://xpandnugetserver.azurewebsites.net/) as a nuget package source in VS.`r`n`r`n. Thanks a lot for your contribution."
+$msg
+UpdateIssues "eXpand.lab" "18.2"
