fe9d2b020c7ca139d23d30a489c1afe8b3415453
apobekiaris <apostolis.bekiaris@gmail.com>
2019-04-02 19:21:38 +0300

    Checkpoint-GithubIssue does not notify for lab

--- Scripts/Checkpoint-GitHubIssue.ps1
+++ Scripts/Checkpoint-GitHubIssue.ps1
@@ -1,14 +1,14 @@
 param(
     $GithubUserName ,
-    $GithubPass ,
-    $BuildRepository
+    $GithubPass 
 )
-$VerbosePreference = "continue"
+
 $yaml = @"
 - Name: XpandPosh
-  Version: 1.5.3
+  Version: 1.7.0
 "@
 & "$PSScriptRoot\Install-Module.ps1" $yaml
+$VerbosePreference = "continue"
 function UpdateIssues ($Repository,$Branch) {
     
     $cred = @{
@@ -24,15 +24,21 @@ function UpdateIssues ($Repository,$Branch) {
     } + $cred
     $commitArgs
     $commitIssues = Get-GitHubCommitIssue @commitArgs 
-    $commitIssues
+    $commitIssues.GitHubCommit.Commit.Message
     if ($commitIssues) {
         $milestone = Get-GitHubMilestone -Repository eXpand -Latest @cred
-        $milestone
+        $milestone.Title
         Checkpoint-GithubIssue -CommitIssues $commitIssues -Message $msg @cred |ForEach-Object {
             if ($_) {
                 $_
-                if ($_.Number) {
-                    Update-GitHubIssue -IssueNumber $_.Number -Repository eXpand -MileStoneTitle $milestone
+                if ($_.IssueNumber) {
+                    $updateArgs=@{
+                        IssueNumber =$_.IssueNumber 
+                        Repository ="eXpand" 
+                        MileStoneTitle =$milestone.Title
+                    }+$cred
+                    $updateArgs
+                    Update-GitHubIssue  @updateArgs
                 }
             }
         }
@@ -40,10 +46,10 @@ function UpdateIssues ($Repository,$Branch) {
 }
 
 
-$msg="The DevExpress.XAF includes commit {Commits} that relate to this task. Please update the related Nuget packages and test if issues is addressed. These are nightly nuget packages available only from our [NugetServer](https://xpandnugetserver.azurewebsites.net/)."
+$msg="The [DevExpress.XAF](https://github.com/eXpandFramework/DevExpress.XAF) repository includes commit {Commits} that relate to this task. Please update the related Nuget packages and test if issues is addressed. These are nightly nuget packages available only from our [NugetServer](https://xpandnugetserver.azurewebsites.net/).`r`n`r`nThanks a lot for your contribution."
 UpdateIssues "DevExpress.XAF" "lab"
 
 $version = Get-XpandVersion -Lab 
-$msg = "eXpand.lab Installer [$version](https://github.com/eXpandFramework/lab/releases/$version) includes commit {Commits} that relate to this task. Please test if it addresses the problem. If you use nuget add our `LAB` [NugetServer](https://xpandnugetserver.azurewebsites.net/) as a nuget package source in VS.`r`n`r`n. Thanks a lot for your contribution."
+$msg = "eXpand.lab release [$version](https://github.com/eXpandFramework/lab/releases/$version) includes commit {Commits} that relate to this task. Please test if it addresses the problem. If you use nuget add our `LAB` [NugetServer](https://xpandnugetserver.azurewebsites.net/) as a nuget package source in VS.`r`n`r`nThanks a lot for your contribution."
 $msg
 UpdateIssues "eXpand.lab" "18.2"
