29b68346e6bdd16afcc6fb5bf4e30b6ac88d736d
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-06-25 21:22:42 +0300

    Xpand-GitHub-Release: Missed Notes generation

--- Scripts/Xpand-GitHub-Release.ps1
+++ Scripts/Xpand-GitHub-Release.ps1
@@ -12,27 +12,33 @@ if (Test-Path $Root) {
 New-Item $Root -ItemType Directory
 $yaml = @"
 - Name: XpandPwsh
-  Version: 0.9.11
+  Version: 0.9.13
 - Name: VSTeam
   Version: 6.2.1
 "@
 & "$PSScriptRoot\Install-Module.ps1" $yaml
+
 function UpdateHistory {
     param(
         $CommitIssues,
         $Release
     )
-    Set-Location $env:TEMP
+    
     $directory = [System.Guid]::NewGuid()
-    New-Item $directory -ItemType Directory
-    "$env:TEMP\$directory"
+    New-Item -Path "$env:TEMP\$directory" -ItemType Directory
+    Set-Location "$env:TEMP\$directory"
     $url = "https://$GithubUserName`:$GithubPass@github.com/eXpandFramework/eXpand.git"
     git clone $url
-    Set-Location ".\eXpand\ReleaseNotesHistory"
+    Set-Location "$env:TEMP\$directory\eXpand\ReleaseNotesHistory"
 
-    git config --global user.email $GitUserEmail
-    git config --global user.name "Apostolis Bekiaris"
-    $releaseHistory=$commitIssues | ForEach-Object {
+    try {
+        git config --global user.email $GitUserEmail
+        git config --global user.name "Apostolis Bekiaris"
+    }
+    catch {
+        
+    }
+    $releaseHistory = $commitIssues | ForEach-Object {
         $issues = $_.Issues.Number -join ", "
         $message = $_.GitHubCommit.Commit.Message
         $_.Issues.Number | ForEach-Object {
@@ -50,7 +56,9 @@ function UpdateHistory {
             } 
         }
     }
-    $import=Import-Csv .\History.csv|ForEach-Object{
+    "releaseHistory:"
+    $releaseHistory|Write-Host
+    $import = Import-Csv .\History.csv | ForEach-Object {
         [PSCustomObject]@{
             Release = $_.Release
             Issues  = $_.issues
@@ -59,21 +67,24 @@ function UpdateHistory {
             Sha     = $_.GitHubCommit.Sha
         } 
     }
-    Remove-Item .\History.csv
-    ($releaseHistory+$import)|Export-Csv .\History.csv -NoTypeInformation
     
+    ($releaseHistory + $import) | Export-Csv "$env:TEMP\$directory\eXpand\ReleaseNotesHistory\History.csv" -NoTypeInformation 
+    "Exported"
+    $ErrorActionPreference="continue"
     git add -A 
     git commit -m "Update $Release History"
     git push -f origin
+    $ErrorActionPreference="Stop"
+    "Update $Release History"
 }
 Set-VSTeamAccount -Account eXpandDevOps -PersonalAccessToken $AzureToken
-$allBuilds=Get-VSTeamBuild -ProjectName eXpandFramework
-$labBuild = $allBuilds|Where-Object {$_.DefinitionName -eq "Xpand-Lab" -and $_.Result -eq "succeeded"}|Select-Object -first 1
-$betaBuild = $allBuilds|Where-Object {$_.DefinitionName -eq "Beta" -and $_.Result -eq "succeeded"}|Select-Object -first 1
-if ([version]$betaBuild.buildNumber -gt [version]$labBuild.buildNumber){
-    $labBuild=$betaBuild
+$allBuilds = Get-VSTeamBuild -ProjectName eXpandFramework
+$labBuild = $allBuilds | Where-Object { $_.DefinitionName -eq "Xpand-Lab" -and $_.Result -eq "succeeded" } | Select-Object -first 1
+$betaBuild = $allBuilds | Where-Object { $_.DefinitionName -eq "Beta" -and $_.Result -eq "succeeded" } | Select-Object -first 1
+if ([version]$betaBuild.buildNumber -gt [version]$labBuild.buildNumber) {
+    $labBuild = $betaBuild
 }
-$releaseBuild = $allBuilds|Where-Object {$_.DefinitionName -eq "Xpand-Release" -and $_.Result -eq "succeeded"}|Select-Object -first 1
+$releaseBuild = $allBuilds | Where-Object { $_.DefinitionName -eq "Xpand-Release" -and $_.Result -eq "succeeded" } | Select-Object -first 1
 $labBuild.buildNumber
 $releaseBuild.BuildNumber
 $version = $labBuild.BuildNumber
@@ -93,7 +104,7 @@ $uri
 Invoke-WebRequest -Uri $uri -OutFile "$Root\artifact.zip"
 Expand-Archive "$Root\artifact.zip" -destinationpath $Root\artifacts 
 
-$files = Get-ChildItem $Root\artifacts\Xpand.v$version|Select-Object -ExpandProperty FullName
+$files = Get-ChildItem $Root\artifacts\Xpand.v$version | Select-Object -ExpandProperty FullName
 $files
 if (!$files) {
     throw "No artifacts found"
@@ -103,31 +114,32 @@ $cred = @{
     Pass         = $GitHubPass 
     Organization = "eXpandFramework"
 }
-$commitIssues = Get-GitHubCommitIssue -Repository1 eXpand -Repository2 $targetRepo @cred -Since (Get-Date).AddDays(-6)
+$date = (Get-GitHubRelease -Repository $targetRepo @cred | Select-Object -First 1).PublishedAt
+$commitIssues = Get-GitHubCommitIssue -Repository1 eXpand -Repository2 $targetRepo @cred -Since $date.DateTime
 "commitIssues:"
 $commitIssues.GitHubCommit.Commit.Message
-if ($targetRepo -eq "eXpand.lab") {
-    $lastLabRelease = Get-GitHubRelease -Repository $targetRepo @cred | Select-Object -First 2 -Skip 1
-    "lastLabRelease=$($lastLabRelease.TagName)"
-    $releaseDate = $lastLabRelease.PublishedAt.DateTime
-    "releaseDate=$releaseDate"
-    $commitIssues = $commitIssues | Where-Object { $releaseDate -lt $_.Githubcommit.Commit.Author.Date.DateTime }
-    "commitIssues:"
-}
-    $commitIssues.GitHubCommit.Commit.Message
+# if ($targetRepo -eq "eXpand.lab") {
+#     $lastLabRelease = Get-GitHubRelease -Repository $targetRepo @cred | Select-Object -First 2 -Skip 1
+#     "lastLabRelease=$($lastLabRelease.TagName)"
+#     $releaseDate = $lastLabRelease.PublishedAt.DateTime
+#     "releaseDate=$releaseDate"
+#     $commitIssues = $commitIssues | Where-Object { $releaseDate -lt $_.Githubcommit.Commit.Author.Date.DateTime }
+#     "commitIssues:"
+# }
+$commitIssues.GitHubCommit.Commit.Message
 
 if ($commitIssues) {
-    if ($targetRepo -eq "eXpand"){
+    if ($targetRepo -eq "eXpand") {
         UpdateHistory $commitIssues $version
     }
     
     $notes = New-GithubReleaseNotes -CommitIssues $commitIssues 
     "notes=$notes"
     
-    $authors = $commitIssues.githubcommit.commit.author|Where-Object{$_.Name -ne "Apostolis Bekiaris"} | ForEach-Object { "[$($_.Name)](https://github.com/$($_.Name.Replace(' ',''))), " } | Select-Object -Unique
+    $authors = $commitIssues.githubcommit.commit.author | Where-Object { $_.Name -ne "Apostolis Bekiaris" } | ForEach-Object { "[$($_.Name)](https://github.com/$($_.Name.Replace(' ',''))), " } | Select-Object -Unique
     "authors=$authors"
-    if ($commitIssues.Issues){
-        $commentsUsers = $commitIssues.Issues | Get-GitHubIssueComment -Repository eXpand @cred | ForEach-Object {$_.User}|Where-Object{$_.Login -ne "eXpand"}
+    if ($commitIssues.Issues) {
+        $commentsUsers = $commitIssues.Issues | Get-GitHubIssueComment -Repository eXpand @cred | ForEach-Object { $_.User } | Where-Object { $_.Login -ne "eXpand" }
         "commentsUsers=$commentsUsers"
     }
     
@@ -155,7 +167,7 @@ Set-ExecutionPolicy Bypass -Scope Process -Force;iex `"`$(([System.Net.WebClient
 if (!$notes) {
     $notes = "There are no enhancements or bugs."
 }
-$badgeVersion="$($version.Major).$($version.Minor).$($version.Build)"
+$badgeVersion = "$($version.Major).$($version.Minor).$($version.Build)"
 $notes = "![GitHub Releases (by Release)](https://img.shields.io/github/downloads/expandframework/$targetRepo/$version/total.svg) ![Custom badge](https://img.shields.io/endpoint.svg?label=Nuget&url=https%3A%2F%2Fxpandnugetstats.azurewebsites.net%2Fapi%2Ftotals%2Fversion%3Fid%3DeXpand%26version%3D$badgeVersion)`r`n`r`nThis release is compiled against DevExpress.XAF v$dxversion.`r`n$usernotes`r`n`r`n[<img src='https://img.shields.io/badge/Search-ReleaseHistory-green.svg'/>](https://github.com/eXpandFramework/eXpand/tree/master/ReleaseNotesHistory)`r`n$notes`r`n`r`n$installerNotes"
 $publishArgs = (@{
         Repository   = $targetRepo
