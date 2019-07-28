7c127289d2fe293e297248b16083ce3e9a0e3807
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2019-07-29 00:42:45 +0300

    Close-GitHubIssue-ForAge keepwhen assigned

--- Scripts/Close-GitHubIssue-ForAge.ps1
+++ Scripts/Close-GitHubIssue-ForAge.ps1
@@ -2,5 +2,5 @@ param(
     $GitHubUserName,
     $GitHubPass
 )
-Install-Module XpandPwsh -RequiredVersion 0.7.1 -Scope CurrentUser -Force -Repository PSGallery
-Close-GithubIssue -Repository1 eXpand  -Owner $GithubUserName -Pass $GithubPass -Organization eXpandFramework
+Install-Module XpandPwsh -RequiredVersion 0.13.1 -Scope CurrentUser -Force -Repository PSGallery
+Close-GithubIssue -Repository1 eXpand  -Owner $GithubUserName -Pass $GithubPass -Organization eXpandFramework -KeepWhenAssignees
