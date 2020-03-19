7c79946cfc2b854d8cdc262c07cdac6a240d3391
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-03-19 13:17:40 +0200

    labels issues: does not match labels

--- Scripts/Label-Issues.ps1
+++ Scripts/Label-Issues.ps1
@@ -60,7 +60,10 @@ function Add-IssuePriority {
         "1. [$($_.Name)]($url)`r`n"
     }
     $labelsText = "We will try to answer all questions that do not require research within 24hr.`r`nTo prioritize cases that require research we use the following labels **in order**. For all other issues the posting time is respected.`r`n$labelsText`r`n`r`n**This case is prioritized.**"
-    (Get-GitHubIssue @iArgs -Labels $gitHubLabels | Where-Object { !($_.Labels.Name | Select-String priority) -and $_.Assignee.login -eq "apobekiaris" }) | ForEach-Object { 
+    $issues=$gitHubLabels.Name|ForEach-Object{
+        Get-GitHubIssue @iArgs -Labels $_    
+    }|Sort-Object Number -Unique
+    ($issues| Where-Object { !($_.Labels.Name | Select-String priority) -and $_.Assignee.login -eq "apobekiaris" }) | ForEach-Object { 
         $issueNumber = $_.Number
         $issueTitle = $_.Title
         $labels = $_.Labels.Name
