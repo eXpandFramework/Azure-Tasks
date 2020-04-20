7bea455dd4f6817e4a3a9afc4f1f64f1f4bb31af
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-04-20 20:48:06 +0300

    PS7 issues

--- Scripts/Install-Module.ps1
+++ Scripts/Install-Module.ps1
@@ -4,9 +4,9 @@ param(
 
 if (!(Get-Module powershell-yaml -ListAvailable)) {
     Install-Module powershell-yaml -RequiredVersion 0.4.0 -Scope CurrentUser -Force -Repository PSGallery
-    Import-Module powershell-yaml
+    
 }
-
+Import-Module powershell-yaml
 ($Yaml | ConvertFrom-Yaml) | ForEach-Object {
     if (!(Get-Module $_.Name -ListAvailable)) {
         $a = @{
