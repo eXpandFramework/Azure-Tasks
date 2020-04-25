ec78443291d55bf1ef8135c79da4336f75584fb6
Apostolis Bekiaris <apostolis.bekiaris@gmail.com>
2020-04-25 17:37:27 +0300

    Twitter: Xpandpwsh update

--- Scripts/Twitter.ps1
+++ Scripts/Twitter.ps1
@@ -17,7 +17,7 @@ $ErrorActionPreference="stop"
 Set-Location $PSScriptRoot
 $yaml = @"
 - Name: XpandPwsh
-  Version: 1.201.28.2
+  Version: 1.201.28.3
 "@
 & "$PSScriptRoot\Install-Module.ps1" $yaml
 
