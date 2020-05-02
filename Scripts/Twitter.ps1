2620bf5eac62350568602800807be6a270b06494
apobekiaris <apostolis.bekiaris@gmail.com>
2020-05-02 12:25:08 +0300

    Twitter updates

--- Scripts/Twitter.ps1
+++ Scripts/Twitter.ps1
@@ -7,8 +7,8 @@ param(
     $MyTwitterAPISecret=$env:MyTwitterAPISecret,
     $MyTwitterAccessToken=$env:MyTwitterAccessToken,
     $MyTwitterAccessTokenSecret=$env:MyTwitterAccessTokenSecret,
-    $ScriptName="NugetPackagesTwitter",
-    # $ScriptName="XpandPwshTwitter",
+    # $ScriptName="NugetPackagesTwitter",
+    $ScriptName="XpandPwshTwitter",
     $GitHubToken=$env:GitHubToken,
     $GitUserEmail=$env:GitUserEmail
 )
@@ -18,7 +18,7 @@ Set-Location $PSScriptRoot
 # Get-ChildItem  C:\npm\prefix\node_modules\webpack\node_modules\fsevents
 $yaml = @"
 - Name: XpandPwsh
-  Version: 1.201.31.17
+  Version: 1.201.31.20
 "@
 & "$PSScriptRoot\Install-Module.ps1" $yaml
 
