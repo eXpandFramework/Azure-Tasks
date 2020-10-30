3e496202c339bfca298107aedfac4af6ee54c0d0
apobekiaris <apostolis.bekiaris@gmail.com>
2020-10-31 01:38:02 +0200

    Close-GitHubIssue-ForAge

--- Scripts/Close-GitHubIssue-ForAge.ps1
+++ Scripts/Close-GitHubIssue-ForAge.ps1
@@ -5,7 +5,7 @@ param(
 
 $yaml = @"
 - Name: XpandPwsh
-  Version: 1.201.25.7
+  Version: 1.202.44.2
 "@
 & "$PSScriptRoot\Install-Module.ps1" $yaml
-Close-GithubIssue -Repository1 eXpand  -Token $GithubPass -Organization eXpandFramework -KeepWhenAssignees
+Close-GithubIssue -Repository1 eXpand  -Token $GithubPass -Organization eXpandFramework -KeepWhenAssignees -top 1
